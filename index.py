68a525e26be984da2b33957f902e2dd3db2f314e
LucasDantas10 <lucas.dantas1078@gmail.com>
2026-05-22 10:28:23 -0300

    Commit Python

---
+++ index.py
@@ -0,0 +1,9 @@
+import math
+x = float(input("Digite o número: "))
+y = float(input("Digite o segundo número: "))
+print(math.pow(x, y))
+#Raiz quadrada
+print(math.sqrt(36))
+#Valor de pi
+print(math.pi)
+
